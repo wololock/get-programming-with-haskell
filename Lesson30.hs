d53ee1755d017ebb8afbb869d2e91be21632f73a
Szymon Stepniak <szymon.stepniak@gmail.com>
2020-01-11 23:54:48 +0100

    lesson 30 updated

--- Lesson30.hs
+++ Lesson30.hs
@@ -19,3 +19,27 @@ creditsDB = Map.fromList [("nYarlathoTep", 2000)
     , ("rcarter1919", 12)
     , ("xCTHULHUx", 50000)
     , ("yogSOThoth", 150000)]
+
+lookupUserName :: GamerId -> Maybe UserName
+lookupUserName id = Map.lookup id userNameDB
+
+lookupCredits :: UserName -> Maybe PlayerCredits
+lookupCredits username = Map.lookup username creditsDB
+
+creditsFromId :: GamerId -> Maybe PlayerCredits
+creditsFromId id = lookupUserName id >>= lookupCredits
+
+echo :: IO ()
+echo = getLine >>= putStrLn
+
+-- Q30.3
+-- Combine readInt and printDouble (defined next) into a single IO action:
+
+readInt :: IO Int
+readInt = read <$> getLine
+
+printDouble :: Int -> IO ()
+printDouble n = print (n*2)
+
+combinedReadAndPrintDouble :: IO ()
+combinedReadAndPrintDouble = readInt >>= printDouble
